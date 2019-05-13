e1e74632f1b4be9ce0a3d24f80b598ed4f1ebab9
I9hdkill <jonathanfriedrich@live.de>
2019-05-13 23:17:19 +0200

    Bandaid fix for potential null player in FaintListener

--- src/main/java/rs/expand/pixelmonbroadcasts/listeners/PokemonFaintListener.java
+++ src/main/java/rs/expand/pixelmonbroadcasts/listeners/PokemonFaintListener.java
@@ -2,6 +2,9 @@
 package rs.expand.pixelmonbroadcasts.listeners;
 
 import com.pixelmonmod.pixelmon.api.events.PixelmonFaintEvent;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import rs.expand.pixelmonbroadcasts.enums.EventData;
 
@@ -21,16 +24,21 @@ public class PokemonFaintListener
             final String localizedName = event.pokemon.getLocalizedName();
 
             // If we're in a localized setup, format a string for logging both names.
-            final String nameString =
-                    baseName.equals(localizedName) ? baseName : baseName + " (" + localizedName + ")";
+            final String nameString = baseName.equals(localizedName) ? baseName : baseName + " (" + localizedName + ")";
+            EntityPlayerMP playerMP;
+            if (event.player != null) {
+                playerMP = event.player;
+            } else {
+                playerMP = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUUID(event.pokemon.getOwnerId());
+            }
 
             // Send a log message if we're set up to do logging for this event.
-            logEvent(EventData.Others.FAINT, event.player.getEntityWorld().getWorldInfo().getWorldName(),
-                    event.player.getPosition(), event.player.getName(), nameString);
+            logEvent(EventData.Others.FAINT, playerMP.getEntityWorld().getWorldInfo().getWorldName(),
+                    playerMP.getPosition(), playerMP.getName(), nameString);
 
             // Send enabled broadcasts to people who should receive them.
             iterateAndBroadcast(EventData.Others.FAINT,
-                    event.pokemon, null, event.player, null);
+                    event.pokemon, null, playerMP, null);
         }
     }
 }
